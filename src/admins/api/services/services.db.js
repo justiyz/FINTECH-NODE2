f5b4e5d190ac53626c263904a48d7b6a0159e9d9
Akinpelumi Akintunde <akintundeakinpelumi@gmail.com>
2023-02-06 14:36:19 +0100

    add promise to db call js docs

--- src/admins/api/services/services.db.js
+++ src/admins/api/services/services.db.js
@@ -4,7 +4,7 @@ import { db } from '../../../users/config/db';
  * Process db.any calls to the database
  * @param {String} query - The request from the endpoint.
  * @param {Array} payload - The response returned by the method.
- * @returns { JSON } - A response with the queried data or no data
+ * @returns { Promise<JSON> } - A Promise response with the queried data or no data
  * @memberof AdminPostgresDbService
  */
 export const processAnyData = (query, payload) => db.any(query, payload);
@@ -13,7 +13,7 @@ export const processAnyData = (query, payload) => db.any(query, payload);
  * Process db.none calls to the database
  * @param {String} query - The request from the endpoint.
  * @param {Array} payload - The response returned by the method.
- * @returns { JSON } - no queried value
+ * @returns { Promise<JSON> } - no queried value
  * @memberof AdminPostgresDbService
  */
 export const processNoneData = (query, payload) => db.none(query, payload);
@@ -22,7 +22,7 @@ export const processNoneData = (query, payload) => db.none(query, payload);
  * Process db.oneOrNone calls to the database
  * @param {String} query - The request from the endpoint.
  * @param {Array} payload - The response returned by the method.
- * @returns { JSON } - A response with the queried data or no data
+ * @returns { Promise<JSON> } - A Promise response with the queried data or no data
  * @memberof AdminPostgresDbService
  */
 export const processOneOrNoneData = (query, payload) => db.oneOrNone(query, payload);
