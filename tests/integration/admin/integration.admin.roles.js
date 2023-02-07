ff89e2c87b96d92e7044996f8d2b6576cad94cea
Rashidat Sikiru <rashidatsikiru@Rashidats-MacBook-Pro.local>
2023-02-07 16:08:06 +0100

    refactor test

--- tests/integration/admin/integration.admin.roles.js
+++ tests/integration/admin/integration.admin.roles.js
@@ -999,26 +999,6 @@ describe('Admin roles', () => {
           done();
         });
     });
-    it('Should return error if invalid token is set', (done) => {
-      chai.request(app)
-        .get('/api/v1/admin/role/fetch-admins')
-        .set({
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.SEEDFI_SUPER_ADMIN_ACCESS_TOKEN}6t7689`
-        })
-        .query({
-          search: 'janet okoro'
-        })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(401);
-          expect(res.body).to.have.property('message');
-          expect(res.body).to.have.property('status');
-          expect(res.body.message).to.equal('invalid signature');
-          expect(res.body.error).to.equal('UNAUTHORIZED');
-          expect(res.body.status).to.equal(enums.ERROR_STATUS);
-          done();
-        });
-    });
     it('Should filter admins by the role status ', (done) => {
       chai.request(app)
         .get('/api/v1/admin/role/fetch-admins')
@@ -1035,26 +1015,6 @@ describe('Admin roles', () => {
           expect(res.body).to.have.property('status');
           expect(res.body.message).to.equal(enums.ADMINS_PER_ROLES_FETCHED_SUCCESSFULLY);
           expect(res.body.status).to.equal(enums.SUCCESS_STATUS);
-          done();
-        });
-    });
-    it('Should return error if invalid token is set', (done) => {
-      chai.request(app)
-        .get('/api/v1/admin/role/fetch-admins')
-        .set({
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.SEEDFI_SUPER_ADMIN_ACCESS_TOKEN}6t7689`
-        })
-        .query({
-          status: 'active'
-        })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(401);
-          expect(res.body).to.have.property('message');
-          expect(res.body).to.have.property('status');
-          expect(res.body.message).to.equal('invalid signature');
-          expect(res.body.error).to.equal('UNAUTHORIZED');
-          expect(res.body.status).to.equal(enums.ERROR_STATUS);
           done();
         });
     });
@@ -1075,27 +1035,6 @@ describe('Admin roles', () => {
           expect(res.body).to.have.property('status');
           expect(res.body.message).to.equal(enums.ADMINS_PER_ROLES_FETCHED_SUCCESSFULLY);
           expect(res.body.status).to.equal(enums.SUCCESS_STATUS);
-          done();
-        });
-    });
-    it('Should return error if invalid token is set', (done) => {
-      chai.request(app)
-        .get('/api/v1/admin/role/fetch-admins')
-        .set({
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.SEEDFI_SUPER_ADMIN_ACCESS_TOKEN}6t7689`
-        })
-        .query({
-          from_date: '2023-01-13 23:03:09.875717',
-          to_date: '2023-01-14 23:03:09.875717+01'
-        })
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(401);
-          expect(res.body).to.have.property('message');
-          expect(res.body).to.have.property('status');
-          expect(res.body.message).to.equal('invalid signature');
-          expect(res.body.error).to.equal('UNAUTHORIZED');
-          expect(res.body.status).to.equal(enums.ERROR_STATUS);
           done();
         });
     });
@@ -1120,7 +1059,6 @@ describe('Admin roles', () => {
           done();
         });
     });
-
   });
 });
 
