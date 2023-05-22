7893b7c259d96bc62a14bcc900306f9de124b3d6
miracle <liegeubani@gmail.com>
2023-05-22 10:46:02 +0100

    add date filter to query

--- src/admins/api/queries/queries.bvn.js
+++ src/admins/api/queries/queries.bvn.js
@@ -76,7 +76,8 @@ export default {
     OR TRIM(CONCAT(last_name, ' ', middle_name, ' ', first_name)) ILIKE TRIM($1)
     OR TRIM(CONCAT(middle_name, ' ', first_name, ' ', last_name)) ILIKE TRIM($1) 
     OR TRIM(CONCAT(middle_name, ' ', last_name, ' ', first_name)) ILIKE TRIM($1)
-    OR $1 IS NULL)`,
+    OR $1 IS NULL)
+    AND ((created_at::DATE BETWEEN $2::DATE AND $3::DATE) OR ($2 IS NULL AND $3 IS NULL)) `,
 
   updateUnblackListedBvn: ` 
   INSERT INTO unblacklisted_bvns(
