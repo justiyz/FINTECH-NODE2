553dc296d7de0605a34b40743487e3f0c06f53f7
Akinpelumi Akintunde <akintundeakinpelumi@gmail.com>
2023-09-06 23:21:56 +0100

    hot fixes for address verification server error

--- tests/unit/controllers/unit.controllers.user.js
+++ tests/unit/controllers/unit.controllers.user.js
@@ -181,12 +181,6 @@ describe('', () => {
       expect(data.code).to.equal(500);
       expect(data.error).to.equal('INTERNAL_SERVER_ERROR');
     });
-    it('should call initiateAddressVerification', async() => {
-      const req = { user: '', body: '' };
-      const data = await UserController.initiateAddressVerification(req, res, next);
-      expect(data.code).to.equal(500);
-      expect(data.error).to.equal('INTERNAL_SERVER_ERROR');
-    });
     it('should call updateUploadedUtilityBill', async() => {
       const req = { user: '', document: '' };
       const data = await UserController.updateUploadedUtilityBill(req, res, next);
