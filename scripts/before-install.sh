2a5afac103431bc5988e9ab2fb2ebfbb39b7da43
eloka2023 <139769500+eloka2023@users.noreply.github.com>
2023-10-11 15:28:20 +0100

    update

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define paths
-app_dir="/home/ubuntu/seedfi-backend-test"  # Change this to your app's directory
+app_dir="/home/ubuntu/seedfi-backend-test/seedfi-backend-api"  # Change this to your app's directory
 env_dir="/home/ubuntu/seedfi-backend/" 
 
 # Remove present folder
 rm -rf "$app_dir"
 
-unzip /home/ubuntu/seedfi-app.zip -d "$app_dir"
+unzip /home/ubuntu/seedfi-backend-test/seedfi-app.zip -d "$app_dir"
 
 # Restore .env file from backup directory to app folder
 cp "$env_dir/.env" "$app_dir"
@@ -16,6 +16,6 @@ chown -R ubuntu:ubuntu "$app_dir"
 
 
 # # Run npm install
-cd /home/ubuntu/seedfi-backend-test
+cd /home/ubuntu/seedfi-backend-test/seedfi-backend-api
 sudo docker ps
 sudo docker build -t seedfi-backend-test .
