a920688ee267c276dcfc51f9454903986ca80aad
eloka2023 <139769500+eloka2023@users.noreply.github.com>
2023-11-03 08:44:50 +0100

    update script

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,7 +2,7 @@
 
 # Define paths
 app_dir="/home/ubuntu/seedfi-backend-test/seedfi-backend-api"  # Change this to your app's directory
-env_dir="/home/ubuntu/seedfi-backend/" 
+env_dir="/opt/vars/" 
 
 # Remove present folder
 rm -rf "$app_dir"
@@ -10,6 +10,6 @@ rm -rf "$app_dir"
 unzip /home/ubuntu/seedfi-backend-test/seedfi-app.zip -d "$app_dir"
 
 # Restore .env file from backup directory to app folder
-cp "$env_dir/.env" "$app_dir"
+cp "$env_dir/seedfi.env" "$app_dir/.env"
 
 chown -R ubuntu:ubuntu "$app_dir"
--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -4,6 +4,7 @@
 CONTAINER_NAME='seedfi-backend-test'
 IMAGE_URL="seedfi-backend-test:latest"
 PORT=4500
+ENV="/opt/vars/seedfi.env"
 
 # Restart the container
 echo "Running Hook: start-application.sh"
@@ -19,7 +20,7 @@ sudo docker stop "$CONTAINER_NAME" && sudo docker rm "$CONTAINER_NAME"
 
 # Start new Container
 echo "Running Hook: Starting the new Container"
-sudo docker run --name "$CONTAINER_NAME" -d -p $PORT:4000 "$IMAGE_URL"
+sudo docker run --name "$CONTAINER_NAME" --env-file="$ENV" -d -p $PORT:4000 "$IMAGE_URL"
 
 # Remove Dangling Images
 echo "Running Hook: Removing Dangling Images!!!"
