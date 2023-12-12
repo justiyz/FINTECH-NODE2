b4c8ba60020ce38d9f4b07d270a92e89ea666595
Yusuf Ola <yusufoolalere@gmail.com>
2023-12-12 15:30:01 +0100

    [fix] Nin image upload and decryptUserBVN

--- src/users/config/s3/index.js
+++ src/users/config/s3/index.js
@@ -4,7 +4,7 @@ import config from '../index';
 AWS.config.update({
   region: config.SEEDFI_AMAZON_S3_BUCKET_REGION,
   accessKeyId: config.SEEDFI_AMAZON_S3_ACCESS_KEY_ID,
-  secretAccessKey: config.SEEDFI_AMAZON_S3_SECRET_ACCESS_KEY,
+  secretAccessKey:  config.SEEDFI_AMAZON_S3_SECRET_ACCESS_KEY,
   signatureVersion: 'v4'
 });
 
