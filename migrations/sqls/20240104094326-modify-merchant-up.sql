c4b3df22b76845ee4c21201924a4dc90b5e7dab3
abraham_hanks <ekekhorabraham@gmail.com>
2024-01-04 11:25:49 +0100

    add fees to create merchant

---
+++ migrations/sqls/20240104094326-modify-merchant-up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE merchants ADD COLUMN processing_fee INTEGER DEFAULT 0;
+ALTER TABLE merchants ADD COLUMN insurance_fee INTEGER DEFAULT 0;
+ALTER TABLE merchants ADD COLUMN advisory_fee INTEGER DEFAULT 0;
