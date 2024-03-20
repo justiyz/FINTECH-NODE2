cf431d3821614e58257825fdb06d20c7cad27a97
Tomiiwo Fakinlede <ftomiiwo@gmail.com>
2024-03-20 22:46:31 +0100

    sdjlf

--- migrations/sqls/20240319163243-verifiedBVNInformation-up.sql
+++ migrations/sqls/20240319163243-verifiedBVNInformation-up.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS verified_bvn_records (
-  id SERIAL NUMERIC NOT NULL,
+  id SERIAL NOT NULL,
   record_id VARCHAR PRIMARY KEY UNIQUE DEFAULT ('user-bvn-' || LOWER(REPLACE(CAST(uuid_generate_v1mc() AS VARCHAR(50)), '-',''))),
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   bvn TEXT NOT NULL,
   gender TEXT NOT NULL,
   date_of_birth TEXT NULL,
+  phone_number TEXT NULL,
+  email VARCHAR(255) NOT NULL,
   is_deleted BOOLEAN DEFAULT false,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW()
