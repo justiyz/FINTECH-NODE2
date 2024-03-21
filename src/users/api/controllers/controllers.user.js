8a96452ff6edc68917d0db4fe09cf9a3b5e2eb0f
Tomiiwo Fakinlede <ftomiiwo@gmail.com>
2024-03-21 08:57:51 +0100

    conditionally send OTP

--- src/users/api/controllers/controllers.user.js
+++ src/users/api/controllers/controllers.user.js
@@ -1609,28 +1609,29 @@ export const sendBvnOtp = async(req, res, next) => {
      */
     if(saved_information.length > 0) {
       await reusableBvnInfo(res, bvn, saved_information)
-    }
-    /** return saved_information; */
-    const { data } = await zeehService.zeehBVNVerificationCheck(bvn.trim(), {});
-    if (!data.success) {
-      logger.info(`${enums.CURRENT_TIME_STAMP}, Guest user:::Info: user's bvn verification failed sendBvnOtp.controller.user.js`);
+    } else {
+      /** return saved_information; */
+      const { data } = await zeehService.zeehBVNVerificationCheck(bvn.trim(), {});
+      if (!data.success) {
+        logger.info(`${enums.CURRENT_TIME_STAMP}, Guest user:::Info: user's bvn verification failed sendBvnOtp.controller.user.js`);
 
-      return ApiResponse.error(res, enums.UNABLE_TO_VERIFY_BVN, enums.HTTP_BAD_REQUEST, enums.SEND_BVN_OTP_CONTROLLER);
-    }
-    // compare bvn dob with provided dob information
-    if (dayjs(date_of_birth.trim()).format('YYYY-MM-DD') !== dayjs(data.data.dateOfBirth.trim()).format('YYYY-MM-DD')) {
-      logger.info(`${enums.CURRENT_TIME_STAMP}, ${'Guest user'}:::Info: provided date of birth does not match bvn returned date of birth sendBvnOtp.controller.user.js`);
+        return ApiResponse.error(res, enums.UNABLE_TO_VERIFY_BVN, enums.HTTP_BAD_REQUEST, enums.SEND_BVN_OTP_CONTROLLER);
+      }
+      // compare bvn dob with provided dob information
+      if (dayjs(date_of_birth.trim()).format('YYYY-MM-DD') !== dayjs(data.data.dateOfBirth.trim()).format('YYYY-MM-DD')) {
+        logger.info(`${enums.CURRENT_TIME_STAMP}, ${'Guest user'}:::Info: provided date of birth does not match bvn returned date of birth sendBvnOtp.controller.user.js`);
 
-      return ApiResponse.error(res, enums.USER_BVN_NOT_MATCHING_RETURNED_BVN, enums.HTTP_BAD_REQUEST, enums.SEND_BVN_OTP_CONTROLLER);
-    }
+        return ApiResponse.error(res, enums.USER_BVN_NOT_MATCHING_RETURNED_BVN, enums.HTTP_BAD_REQUEST, enums.SEND_BVN_OTP_CONTROLLER);
+      }
 
-    /** if match, send otp to user */
-    await saveBvnInformation(data);
-    const otpData = await sendOtpToBvnUser(bvn, data.data);
-    if (SEEDFI_NODE_ENV === 'test' || SEEDFI_NODE_ENV === 'development') {
-      return ApiResponse.success(res, enums.VERIFICATION_OTP_RESENT, enums.HTTP_CREATED, {...otpData});
+      /** if match, send otp to user */
+      await saveBvnInformation(data);
+      const otpData = await sendOtpToBvnUser(bvn, data.data);
+      if (SEEDFI_NODE_ENV === 'test' || SEEDFI_NODE_ENV === 'development') {
+        return ApiResponse.success(res, enums.VERIFICATION_OTP_RESENT, enums.HTTP_CREATED, {...otpData});
+      }
+      return ApiResponse.success(res, enums.VERIFICATION_OTP_RESENT, enums.HTTP_CREATED, { ...otpData });
     }
-    return ApiResponse.success(res, enums.VERIFICATION_OTP_RESENT, enums.HTTP_CREATED, { ...otpData });
   } catch (error) {
     return ApiResponse.error(res, enums.UNABLE_TO_PROCESS_BVN, enums.HTTP_BAD_REQUEST, enums.SEND_BVN_OTP_CONTROLLER);
   }
