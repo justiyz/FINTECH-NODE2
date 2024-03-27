8d73edce08e115a644ba28ec10c02a2789182a90
Yusuf Ola <yusufoolalere@gmail.com>
2024-03-27 15:47:31 +0100

    fix enum

--- src/users/lib/enums/lib.enum.messages.js
+++ src/users/lib/enums/lib.enum.messages.js
@@ -467,7 +467,5 @@ export const LOAN_REPAID_SUCCESSFULLY = 'Loan repaid successfully';
 export const LOAN_WAS_NOT_CREATED_BY_ADMIN = 'Loan can not be repaid manually, user should kindly login to repay loan';
 export const MANDATE_ALREADY_ACCEPTED = `Acceptance of mandate for this loan has already been attempted`;
 export const API_VERSION = 'API version returned successfully';
-export const COMMERCIAL_BANK_REQUIRED = `Change your bank account to a commercial bank account to proceed`;
-
 
 // export const FAILED_TO_CREATE_TICKET_SUBSCRIPTION = 'failed to create ticket subscription';
